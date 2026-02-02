4faa7bea4480da996644b515834c56a0e786ea9d
racheldean1 <racheldean1@arizona.edu>
2026-02-01 20:01:18 -0700

    Update rod_energy.cpp

--- csrc/rod_energy.cpp
+++ csrc/rod_energy.cpp
@@ -170,7 +170,7 @@ void rod_energy_grad(
             double a0[3] = { get(i, 0),   get(i, 1),   get(i, 2)   }; // endpoints of [i,i+1]
             double a1[3] = { get(ip1, 0), get(ip1, 1), get(ip1, 2) };
             double b0[3] = { get(j, 0),   get(j, 1),   get(j, 2)   }; // endpoints of [j, j+1]
-            double b1[3] = { get(jp1, 0), get(jp1, 1), get(jp1, 2) };
+            double b1[3] = { get(jp1,0), get(jp1,1), get(jp1,2) };
 
             double u = 0.0, v = 0.0; // solving for u,v
             closest_uv(a0, a1, b0, b1, u, v);
